b1de3236160951c6b47def0215077f54cb8b7122
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-06-14 16:03:17 +0100

    import add-0.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    print (a + b)
+#if __name__ == "__main__":
+#	import sys
+#	add(int(sys.argv[1]), int(sys.argv[2]))
+
+

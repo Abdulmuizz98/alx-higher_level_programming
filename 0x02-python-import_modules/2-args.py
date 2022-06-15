5d7a67001fd988583f262cbfabe84ab8c915b5d6
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-06-15 21:01:59 +0100

    prints number and arguments of list

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     import sys
 
     length = len(sys.argv) - 1
-    print("{} ".format(length), end="argument:\n"
-          if length == 1 else "arguments:\n")
+    print("{} ".format(length), end="argument"
+          if length == 1 else "arguments")
+    print("", end=".\n" if length == 0 else ":\n")
     i = 1
     while i <= length:
         print("{}: {}".format(i, sys.argv[i]), end="\n")

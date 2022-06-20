a6743524012d44b553ad8abc377623e8c58ced40
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-06-20 22:53:51 +0100

    safe print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Prints an integer """
+    try:
+        print("{:d}".format(int(value)))
+    except ValueError:
+        return False
+    else:
+        return True

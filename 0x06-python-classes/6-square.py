db5f0c2c78922289d469828d79631cecf7c231fa
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-06-22 04:26:56 +0100

    position and size

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,6 @@ class Square:
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
-
             raise ValueError("size must be >= 0")
         self.__size = value
 
@@ -44,7 +43,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) and len(value) < 2:
+        if not isinstance(value, tuple) or 
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >=0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 

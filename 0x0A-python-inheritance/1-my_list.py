fcdd14a54523de2b86bbb99e76f074b04952da76
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-07-05 01:27:50 +0100

    extend list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,9 @@
 
 class MyList(list):
     """List that inherists from list """
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
         """Prints the list sorted"""

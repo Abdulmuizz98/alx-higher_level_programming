300db23169d8041dc47d200ad69f045c784f364e
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-07-05 01:21:09 +0100

    extend list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module defines an extended list called MyList
+"""
+
+
+class MyList(list):
+    """List that inherists from list """
+
+    def print_sorted(self):
+        """Prints the list sorted"""
+        copy = self[:]
+        copy.sort()
+        print(copy)

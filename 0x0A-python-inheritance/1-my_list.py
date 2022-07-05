7903f3e5def7c3699e32703d3df266e3043898af
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-07-05 01:24:14 +0100

    extend list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the list sorted"""
-        copy = self[:]
-        copy.sort()
-        print(copy)
+        print(sorted(copy))

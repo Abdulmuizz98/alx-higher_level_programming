438bd87eb87dc0cce5c3d5626564adcbf6ec2ea5
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-09-23 03:17:25 +0100

    content_length

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A Bash script that takes in a URL, sends a request
+#+ to that URL, and displays the size of the body of the response
+
+curl -sI localhost | grep Content-Length | sed -r "s/Content-Length: //"

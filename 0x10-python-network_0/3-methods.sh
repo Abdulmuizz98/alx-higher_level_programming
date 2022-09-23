843a35c4935a079affb82254293635b30a3a26e5
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-09-23 05:30:35 +0100

    post parameters

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# A Bash script that that takes in a URL and displays all HTTP methods the server will accept.
 curl -sI -X OPTIONS "$1" | grep -i ^Allow: | sed -e "s/Allow: //"

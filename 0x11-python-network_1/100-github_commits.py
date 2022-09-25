8768570549b97f26e3f2a96d4492053e87d4fa56
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-09-25 01:38:23 +0100

    github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
     r = requests.get(url)
     response = r.json()
-    for commit in response:
+    for commit in response[:10]:
         sha = commit.get('sha')
         author = commit.get('commit').get('author').get('name')
         print(f"{sha}: {author}")

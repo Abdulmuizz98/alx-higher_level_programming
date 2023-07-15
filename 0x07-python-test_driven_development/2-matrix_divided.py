da382ebfd660bf8c9a624e4809e43b6446b5e3bd
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-07-15 16:59:33 +0100

    fix: enforce pycodestyle for 2-matrix_divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,17 +10,21 @@ def matrix_divided(matrix, div):
     """Divides all element of a matrix """
     # Validate matrix type
     if type(matrix) is not list:
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-    
+        raise TypeError('matrix must be a matrix (list of lists) \
+                        of integers/floats')
+
     for row in matrix:
         if type(row) is not list:
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError('matrix must be a matrix (list of lists) \
+                            of integers/floats')
         if len(matrix[0]) != len(row):
-            raise TypeError('Each row of the matrix must have the same size')
+            raise TypeError('Each row of the matrix \
+                            must have the same size')
         for el in row:
             if type(el) not in [int, float]:
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-    
+                raise TypeError('matrix must be a matrix \
+                                (list of lists) of integers/floats')
+
     # Validte div type
     if type(div) not in [int, float]:
         raise TypeError('div must be a number')
@@ -30,6 +34,6 @@ def matrix_divided(matrix, div):
         raise OverflowError
     if div != div:
         raise ValueError
-    
+
     new_matrix = [row[:] for row in matrix]
-    return [ [round(elem / div, 2) for elem in row]  for row in new_matrix ]
+    return [[round(elem / div, 2) for elem in row] for row in new_matrix]

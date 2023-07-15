e513a5e6812802a8ec75bfd3621e1130a733e4fd
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-07-16 00:27:23 +0100

    test: add testcase for text_indentation function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,6 +9,8 @@ function ``text_indentation()``.
 def text_indentation(text):
     """Prints a text with 2 new lines after each of these characters: ., ? and :
     """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
     res = text.replace(': ', ':\n\n').replace('. ', '.\n\n')
     res = res.replace('? ', '?\n\n')
     print(res, end="")

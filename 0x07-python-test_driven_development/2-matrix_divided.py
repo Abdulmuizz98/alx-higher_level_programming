80f7da2cb737b1444478d33495cd9959956dea3a
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-07-15 14:40:16 +0100

    feat: implement function say_my_name

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of an
+addition function.
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """Divides all element of a matrix """
+    # Validate matrix type
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        if len(matrix[1]) != len(row):
+            raise TypeError('Each row of the matrix must have the same size') 
+    
+    # Validte div type
+    if type(div) not in [int, float]:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    
+    new_matrix = [row[:] for row in matrix]
+    return [ round(elem / div, 2)  for row in new_matrix for elem in row]

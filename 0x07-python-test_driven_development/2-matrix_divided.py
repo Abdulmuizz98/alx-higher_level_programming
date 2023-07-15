149b1382287f36a45d1f984e8f9719f5d1fba14d
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-07-15 17:07:14 +0100

    fix: enforce pycodestyle for 2-matrix_divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,20 +10,20 @@ def matrix_divided(matrix, div):
     """Divides all element of a matrix """
     # Validate matrix type
     if type(matrix) is not list:
-        raise TypeError('matrix must be a matrix (list of lists)'\
+        raise TypeError('matrix must be a matrix (list of lists)'
                         ' of integers/floats')
 
     for row in matrix:
         if type(row) is not list:
-            raise TypeError('matrix must be a matrix (list of lists)'\
+            raise TypeError('matrix must be a matrix (list of lists)'
                             ' of integers/floats')
         if len(matrix[0]) != len(row):
-            raise TypeError('Each row of the matrix'\
+            raise TypeError('Each row of the matrix'
                             ' must have the same size')
         for el in row:
             if type(el) not in [int, float]:
-                raise TypeError('matrix must be a matrix'\
-                               ' (list of lists) of integers/floats')
+                raise TypeError('matrix must be a matrix'
+                                ' (list of lists) of integers/floats')
 
     # Validte div type
     if type(div) not in [int, float]:
